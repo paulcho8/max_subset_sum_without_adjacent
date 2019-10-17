75f5c467f0e87b347ca34e593faf61660bec5a22
eightseconds <paulseokcho@gmail.com>
2019-10-16 18:54:59 -0700

    Implement linear time solution

--- maxSubsetSumWithoutAdjacent.js
+++ maxSubsetSumWithoutAdjacent.js
@@ -1,3 +1,11 @@
 function maxSubsetSumNoAdjacent(array) {
+    if (!array.length) return 0;
+    if (array.length === 1) return array[0];
+    const maxes = array.slice();
+    maxes[1] = Math.max(array[0], array[1]);
+    for (let i = 2; i < array.length; i++) {
+        maxes[i] = Math.max(maxes[i - 1], maxes[i - 2] + array[i])
+    }
 
+    return maxes[maxes.length - 1]
 }
